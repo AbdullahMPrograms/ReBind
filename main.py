6f55e95e603c8eb9c30585b8467270cd3bae6adb
AbdullahMPrograms <abdullahmalik1385@gmail.com>
2023-12-17 14:49:44 -0500

    Rebindexp will carry all functions to be called from py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 from ahkunwrapped import Script
 
-ahk = Script.from_file(Path('ReBind.ahk'))
+ahk = Script.from_file(Path('ReBindexp.ahk'))
 ahk.call('MyMsg', "Wooo!")
 
 ahk.call('SendKey', '{Space}')
