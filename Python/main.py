8b0eb45a6a86a785924fecd030350abd58dcbfcf
AbdullahMPrograms <abdullahmalik1385@gmail.com>
2024-02-20 14:09:39 -0500

    Main general text working, no focus/wintitles

--- Python/main.py
+++ Python/main.py
@@ -15,24 +15,17 @@ def generate_ahk_core():
 
     ahk_core = ""
     for program, layers in remap_keys["remapped_keys"].items():
+        ahk_core += f'\nProgram: {program}\n'
         for layer, modifiers in layers.items():
             for modifier, keys in modifiers.items():
-                for key, key_info in keys.items():
-                    if isinstance(key_info, dict):
-                        remapped_key = key_info.get("key")
-                        if program == "global":
-                            ahk_core += f'{key}::{remapped_key}\n'
-                        elif modifier:
+                if isinstance(keys, dict) and "key" in keys:  # We're at the key level without a modifier
+                    remapped_key = keys.get("key")
+                    ahk_core += f'{modifier}::{remapped_key}\n'
+                else:  # We're at the modifier level or key level with a modifier
+                    for key, key_info in keys.items():
+                        if isinstance(key_info, dict) and "key" in key_info:
+                            remapped_key = key_info.get("key")
                             ahk_core += f'{modifier} & {key}::{remapped_key}\n'
-                        else:
-                            ahk_core += f'{key}::Sendkey({remapped_key})\n'
-                    else:
-                        if program == "global":
-                            ahk_core += f'{key}::{key_info}\n'
-                        elif modifier:
-                            ahk_core += f'{modifier} & {key}::{key_info}\n'
-                        else:
-                            ahk_core += f'{key}::Sendkey({key_info})\n'
 
     return ahk_core
 
